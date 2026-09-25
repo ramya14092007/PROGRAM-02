c8acf69a4fb4ad1091adb12e4252773454fe2e97
RAMYA S <ramya.s978742@gmail.com>
2026-09-25 12:23:49 +0530

    Update and rename student_solution.sql to answers.sql

--- answers.sql
+++ answers.sql
@@ -1,13 +1,6 @@
 CREATE DATABASE IF NOT EXISTS assignmentdb;
 USE assignmentdb;
 
-CREATE TABLE Student (
-    student_id INT PRIMARY KEY,
-    student_name VARCHAR(100),
-    email VARCHAR(100),
-    department_id INT
-);
-
 CREATE TABLE Department (
     department_id INT PRIMARY KEY,
     department_name VARCHAR(100)
@@ -19,10 +12,15 @@ CREATE TABLE Faculty (
     department_id INT
 );
 
+CREATE TABLE Student (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(100),
+    department_id INT
+);
+
 CREATE TABLE Course (
     course_id INT PRIMARY KEY,
     course_name VARCHAR(100),
     faculty_id INT,
     department_id INT
 );
- 
